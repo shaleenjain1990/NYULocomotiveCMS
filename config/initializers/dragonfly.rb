022bd548890cf97cd8b53c2e0ecaac9c20f8047f
Shaleen Jain <shaleenjain1990@gmail.com>
2014-11-11 16:03:36 -0500

    Locomotive CMS Working State

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,24 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret '1f34251d6693f6c94c5aabc03d7cf6ae7237479a7d18495603bc372bcce1d1a9'
+
+  url_format '/images/dynamic/:job/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NYULocomotiveCMS::Application.config.secret_token = '1c83cc5d17ab5d384bb6295de12c261e57e2dbbfa0fb23c3e2f490247082d4890600be5007f427f8475b95f3f56a4e337f0db1d0f8f2d3079f2797bc9cdda7a7'
